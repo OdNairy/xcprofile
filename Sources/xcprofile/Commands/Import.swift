75ebde17a68e082a6d8fa10c7824d12125a2cbb3
Roman Gardukevich <roman@gardukevich.com>
2020-03-22 23:36:04 +0300

    Option to import only accounts

--- Sources/xcprofile/Commands/Import.swift
+++ Sources/xcprofile/Commands/Import.swift
@@ -8,6 +8,9 @@ struct ImportCommand: ParsableCommand {
     @Argument(help: "Absolute path to .developerprofile")
     var profilePath: String
     
+    @Flag()
+    var accountsOnly: Bool
+    
     func run() throws {
         let profileURL = URL(fileURLWithTildePath: profilePath)
         logger.trace("ProfileURL: \(profileURL.path)", metadata: ["input path":.string(profilePath)])
@@ -20,7 +23,13 @@ struct ImportCommand: ParsableCommand {
         let profile = try encryptedProfile.decompress(password: password)
         logger.trace("Decrypted profile at \(profile.outputURL.path)")
         
-        try profile.import()
+        if accountsOnly {
+            logger.info("Importing accounts settings only")
+            try profile.importAccountsSettings()
+        } else {
+            logger.trace("Importing all data")
+            try profile.import()
+        }
     }
     
     static var configuration = CommandConfiguration(commandName: "import")
